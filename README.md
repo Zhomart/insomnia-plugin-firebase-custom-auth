b1475cc60ca2030326a3fdc0dba4dfd85afcdba6
Zhomart Mukhamejanov <mzhomart@gmail.com>
2020-07-08 23:05:50 -0700

    Initial draft

---
+++ README.md
@@ -0,0 +1,61 @@
+# Firebase Custom Auth idToken Retriever
+
+Read https://firebase.google.com/docs/auth/web/custom-auth for context.
+
+This is a plugin for the [Insomnia REST client](https://insomnia.rest/) to retrieve 
+[idToken](https://firebase.google.com/docs/auth/users#auth_tokens) from Firebase. Firebase
+requires first acquiring 
+[custom token](https://firebase.google.com/docs/auth/admin/create-custom-tokens) and uses it 
+to generate `idToken`.
+
+## Usage
+
+Create a login request to your custom authentication service. Set header 
+`X-Insomnia-Firebase-Custom-Token-Path` to [dot-notation](https://lodash.com/docs/4.17.15#get) 
+path to the __Firebase Custom Token__. The plugin automatically stores it and uses it to
+retrieve `idToken`.
+
+![loginAndGetCustomToken.png](https://raw.githubusercontent.com/Zhomart/insomnia-plugin-firebase-custom-auth/master/screenshot/loginAndGetCustomToken.png)
+
+In environment settings (CTRL+e/CMD+e), create an environment variable and set the value
+to the `Firebase idToken` tag (CTRL+SPACE to find the tag).
+
+![createIdTokenTag.png.png](https://raw.githubusercontent.com/Zhomart/insomnia-plugin-firebase-custom-auth/master/screenshot/createIdTokenTag.png.png)
+
+Configure the `Firebase idToken` tag. Set `customTokenOrEmptyToUseStored` only if you
+want to override the stored custom token. It automatically gets `idToken` from Firebase and caches it.
+
+![configuringIdTokenTag.png](https://raw.githubusercontent.com/Zhomart/insomnia-plugin-firebase-custom-auth/master/screenshot/configuringIdTokenTag.png)
+
+Use the `idToken` in your requests.
+
+![useIdToken.png](https://raw.githubusercontent.com/Zhomart/insomnia-plugin-firebase-custom-auth/master/screenshot/useIdToken.png)
+
+Click on `Reset Firebase Custom Token` to remove stored __custom token__ and __idToken__.
+
+![resetTokens.png](https://raw.githubusercontent.com/Zhomart/insomnia-plugin-firebase-custom-auth/master/screenshot/resetTokens.png)
+
+
+# License
+
+    MIT License
+
+    Copyright (c) 2020 Zhomart Mukhamejanov
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
---
+++ main.js
@@ -0,0 +1,119 @@
+const nanoajax = require("nanoajax");
+const jwtDecode = require("jwt-decode");
+const _ = require('lodash');
+
+const STORE_CUSTOM_TOKEN_KEY = "customToken"; // Firebase Custom Token (JWT)
+const STORE_ID_TOKEN_KEY = "idToken"; // Firebase idToken (JWT)
+
+async function nanoajaxAsync(options) {
+  return new Promise((resolve, reject) => {
+    nanoajax.ajax(options, function (code, responseText, request) {
+      if (code === 200) {
+        resolve(responseText);
+      } else {
+        reject(code, responseText);
+      }
+    });
+  });
+}
+
+module.exports.templateTags = [
+  {
+    name: "idToken",
+    displayName: "Firebase idToken",
+    description:
+      "Fetches idToken from Firebase using customToken. Note both tokens have 1 hour lifetime.",
+
+    args: [
+      {
+        displayName: "customTokenOrEmptyToUseStored",
+        description:
+          "Force use this custom token instead. If empty, use the stored one.",
+        type: "string",
+        defaultValue: null,
+      },
+      {
+        displayName: "firebaseKey",
+        description: "Firebase Web API Key (on General Settings page)",
+        type: "string",
+        defaultValue: null,
+      },
+    ],
+
+    async run(context, customTokenOrEmptyForStored, firebaseKey) {
+      const customTokenStored = await context.store.getItem(STORE_CUSTOM_TOKEN_KEY);
+      const idToken = await context.store.getItem(STORE_ID_TOKEN_KEY);
+      const customTokenFinal =
+        customTokenOrEmptyForStored && customTokenOrEmptyForStored.length > 0
+          ? customTokenOrEmptyForStored
+          : customTokenStored;
+      if (idToken && idToken.length > 0) return idToken;
+      if (!customTokenFinal || customTokenFinal.length == 0) {
+        return (
+          "No custom token supplied. Please either use X-Insomnia-Firebase-Custom-Token-Path" +
+          " or set it via params."
+        );
+      }
+      console.log("Making request to get idtoken");
+      const responseRaw = await nanoajaxAsync({
+        url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${firebaseKey}`,
+        method: "POST",
+        body: JSON.stringify({
+          token: customTokenFinal,
+          returnSecureToken: true,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+      const response = JSON.parse(responseRaw);
+      await context.store.setItem(STORE_ID_TOKEN_KEY, response.idToken);
+      return response.idToken;
+    },
+  },
+
+  {
+    name: "customToken",
+    displayName: "Firebase Custom Token",
+    description: "Returns stored custom token",
+
+    async run(context) {
+      const customToken = await context.store.getItem(STORE_CUSTOM_TOKEN_KEY);
+      return customToken;
+    },
+  },
+];
+
+module.exports.responseHooks = [
+  async (context) => {
+    // Expected format: "data.login.firebaseCustomToken"
+    const customTokenPath = context.request.getHeader(
+      "X-Insomnia-Firebase-Custom-Token-Path"
+    );
+    if (!customTokenPath) return;
+    if (context.response.getStatusCode() !== 200) return;
+    console.dir("Parsing response to get customToken");
+    let body = JSON.parse(context.response.getBody());
+    const customToken = _.get(body, customTokenPath)
+    try {
+      const jwt = jwtDecode(customToken);
+      if (!jwt || !jwt.aud) {
+        console.warn("Bad JWT");
+        return;
+      }
+      console.dir("Storing the custom token (it has 1 hour lifetime)");
+      await context.store.setItem(STORE_CUSTOM_TOKEN_KEY, customToken);
+    } catch (error) {
+      console.warn("Failed to decode custom token JWT", error);
+    }
+  },
+];
+
+module.exports.workspaceActions = [
+  {
+    label: "Reset Firebase Custom Tokens",
+    icon: "fa-trash",
+    action: async (context, models) => {
+      await context.store.removeItem(STORE_CUSTOM_TOKEN_KEY);
+      await context.store.removeItem(STORE_ID_TOKEN_KEY);
+    },
+  },
+];
